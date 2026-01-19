d3f31c58d938095e19c98d8ee413e638458b288e
roundRekt <roundrekt@icloud.com>
2026-01-19 22:02:07 +0800

    修复bug

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ export default {
                     )
                 );
             } else return response;
-        }
+        } else serverinfo = await response.json();
 
         if ((request.headers.get("User-Agent") || "").includes("BestHTTP")) {
             let uuid = request.headers.get("Cookie")?.split("uuid=")?.[1]?.split(";")?.[0];
