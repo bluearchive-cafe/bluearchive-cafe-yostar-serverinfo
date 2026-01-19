6647039d522e1f253b8af03868ffdcd105f8e127
roundRekt <roundrekt@icloud.com>
2026-01-19 19:38:24 +0800

    将服务器信息作为静态资源

--- src/index.js
+++ src/index.js
@@ -1,33 +1,20 @@
-import xxhash from "xxhash-wasm";
-
 export default {
     async fetch(request, env, ctx) {
-        const key = new URL(request.url).pathname.slice(1);
-        const upstream = "https://yostar-serverinfo.bluearchiveyostar.com/" + key;
+        const upstream = `https://yostar-serverinfo.bluearchiveyostar.com${new URL(request.url).pathname}`;
         const cafeDomain = "bluearchive.cafe";
         const yostarDomain = "bluearchiveyostar.com";
         const cafeAttributes = `Path=/; Domain=${cafeDomain}; Max-Age=2147483647`;
         const yostarAttributes = `Path=/; Domain=${yostarDomain}; Max-Age=2147483647`;
         const headers = new Headers({ "Content-Type": "application/json; charset=utf-8" });
 
-        if (!key || key.includes("/") || !key.startsWith("r") || !key.endsWith(".json")) return await fetch(upstream);
+        let response = await env.ASSETS.fetch(request);
+        if (!response.ok) response = await fetch(upstream);
+        if (!response.ok) return response;
 
-        let serverinfo;
-        let value = await env.SERVERINFO.get(key);
-        if (!value) {
-            const response = await fetch(upstream);
-            const text = await response.text();
-            const hash = (await xxhash()).h32(text).toString();
-            serverinfo = JSON.parse(text);
-            value = JSON.stringify(serverinfo, null, 2);
-            await env.SERVERINFO.put(key, value);
-            await env.SERVERINFO.put("info.hash", hash);
-        } else serverinfo = JSON.parse(value);
-
-        let uuid, preference;
+        const serverinfo = await response.json();
         if ((request.headers.get("User-Agent") || "").includes("BestHTTP")) {
-            uuid = request.headers.get("Cookie")?.split("uuid=")?.[1]?.split(";")?.[0];
-            preference = uuid && JSON.parse(await env.PREFERENCE.get(uuid) || "null");
+            let uuid = request.headers.get("Cookie")?.split("uuid=")?.[1]?.split(";")?.[0];
+            let preference = uuid && JSON.parse(await env.PREFERENCE.get(uuid) || "null");
             if (!preference) {
                 uuid = crypto.randomUUID();
                 preference = { table: "cn", asset: "jp", media: "jp" };
@@ -35,16 +22,14 @@ export default {
                 headers.append("Set-Cookie", `uuid=${uuid}; ${cafeAttributes}`);
                 headers.append("Set-Cookie", `uuid=${uuid}; ${yostarAttributes}`);
             }
+            const ConnectionGroup = serverinfo.ConnectionGroups[0];
+            const OverrideConnectionGroup = serverinfo.ConnectionGroups[0].OverrideConnectionGroups[1];
+            ConnectionGroup.ManagementDataUrl = ConnectionGroup.ManagementDataUrl.replace(yostarDomain, `${cafeDomain}/${uuid}`);
+            OverrideConnectionGroup.AddressablesCatalogUrlRoot = preference.dev === "true"
+                ? OverrideConnectionGroup.AddressablesCatalogUrlRoot.replace(`prod-clientpatch.${yostarDomain}`, `dev-clientpatch.${cafeDomain}`)
+                : OverrideConnectionGroup.AddressablesCatalogUrlRoot.replace(yostarDomain, `${cafeDomain}/table=${preference.table}/asset=${preference.asset}/media=${preference.media}`);
         }
 
-        const ConnectionGroup = serverinfo.ConnectionGroups[0];
-        const OverrideConnectionGroup = serverinfo.ConnectionGroups[0].OverrideConnectionGroups[1]
-        const managementDataPath = uuid ? `/${uuid}` : "";
-        const addressableCatalogPath = preference ? `/table=${preference.table}/asset=${preference.asset}/media=${preference.media}` : "";
-        ConnectionGroup.ManagementDataUrl = ConnectionGroup.ManagementDataUrl.replace(yostarDomain, cafeDomain + managementDataPath);
-        OverrideConnectionGroup.AddressablesCatalogUrlRoot = preference?.dev === "true"
-            ? OverrideConnectionGroup.AddressablesCatalogUrlRoot.replace(`prod-clientpatch.${yostarDomain}`, `dev-clientpatch.${cafeDomain}`)
-            : OverrideConnectionGroup.AddressablesCatalogUrlRoot.replace(yostarDomain, cafeDomain + addressableCatalogPath);
         return new Response(JSON.stringify(serverinfo, null, 2), { headers });
     },
 };
